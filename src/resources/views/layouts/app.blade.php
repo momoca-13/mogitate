aebc39a83a2cd1b075faf8f24fdf369a28e2204f
momo <tell.me.something.mmk@gmail.com>
2025-03-01 23:51:53 +0900

    変更

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/reset.css" />
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}" />
+    @yield('css')
+</head>
+<body>
+    <header>
+        <h1>mogitate</h1>
+    </header>    
+    
+    <main>
+
+    </main>
+
+    <footer>
+
+
+    </footer>
+</body>
+</html>
