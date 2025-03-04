c6eb75600eda71526bc83e5e6d047d5b7a38c64b
momo <tell.me.something.mmk@gmail.com>
2025-03-04 14:30:39 +0900

    マイグレーション

--- src/app/Http/Controllers/ProductController.php
+++ src/app/Http/Controllers/ProductController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Product;
 
 class ProductController extends Controller
 {
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/reset.css" />
-    <link rel="stylesheet" href="css/style.css" />
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>mogitate</title>
+  <link rel="stylesheet" href="{{ asset('css/reset.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/product.css') }}" />
 </head>
 
 <body>
-    <header class="header">
-        <div class="header__inner">
-            <div class="header__logo" href="/">
-                mogitate
-            </div>
-        </div> 
-    </header>    
-    
+  <header class="header">
+    <div class="header__inner">
+      <a class="header__logo" href="/">mogitate</a>
+      </a>      
+    </div>
+  </header>
+      
     <main>
-
-    </main>
-
-    <footer>
-
-
-    </footer>
+    <img src="{{ asset('storage/masukatto.jpg') }}" alt="代替テキスト" width="374px" height="350px" >
+    <img src="{{ asset('storage/peach.jpg') }}" alt="代替テキスト" width="374px" height="350px" >
+    <img src="{{ asset('storage/suika.jpeg') }}" alt="代替テキスト" width="374px" height="350px" >
+    <img src="{{ asset('storage/orange.jpg') }}" alt="代替テキスト" width="374px" height="350px" >
+    <img src="{{ asset('storage/strawberry.png') }}" alt="代替テキスト" width="374px" height="350px" >
+    <img src="{{ asset('storage/kiwi.jpg') }}" alt="代替テキスト" width="374px" height="350px" >
+</main>
 </body>
+
 </html>
